0f57c7c2ea4ccbeae4a6e9b0e154e80ecd3bb273
holilrhmn <holilrahman10@gmail.com>
2020-04-09 12:55:01 +0800

    Menyalin Isi Folder Build

---
+++ precache-manifest.aeb6f25e49ebd023a01207152f7ba430.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909daf2457ca8bc60670bcfa8717784f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f1afa3d91852d9fe37f",
+    "url": "/static/css/main.770649e7.chunk.css"
+  },
+  {
+    "revision": "3c1e06ed6900e6b103b1",
+    "url": "/static/js/2.1395bae1.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.1395bae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f1afa3d91852d9fe37f",
+    "url": "/static/js/main.32884fcc.chunk.js"
+  },
+  {
+    "revision": "8653c22f6e7f208476d9",
+    "url": "/static/js/runtime-main.aef75ead.js"
+  },
+  {
+    "revision": "cd24ce3c01d60f3fe7e0698473ad011e",
+    "url": "/static/media/jelajah-01.cd24ce3c.png"
+  },
+  {
+    "revision": "f96e0f2da2d8ca8b90ccf0130b68b24c",
+    "url": "/static/media/logo.f96e0f2d.png"
+  }
+]);
